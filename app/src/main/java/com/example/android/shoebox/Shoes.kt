3e6e786b83eae82c083c8b958ac34e2d39e89cd7
Gergana Toshkova <geritoshkova85@gmail.com>
2021-05-18 20:49:23 +0300

    add ShoeListiewModel and Shoe data class to handle the shoes data in the app

---
+++ app/src/main/java/com/example/android/shoebox/Shoes.kt
@@ -0,0 +1,25 @@
+/* Copyright 2021,  Gergana Kirilova
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+
+
+package com.example.android.shoebox
+
+// class to store the data for every shoe pair
+data class Shoes(
+    val shoeName: String, val shoeBrand: String, val shoeSize: MutableList<String>,
+    val shoeDescription: String,
+)
